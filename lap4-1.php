aecd73f614ea45281dd17a7ed22722e90c3d60ca
Tossapol-MT <you@example.com>
2021-01-13 11:08:59 +0700

    update lab4-1.php

---
+++ lap4-1.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <title>การกำหนดค่าตัวแปร</title>
+</head>
+<body>
+    <?php
+        $x = 100;
+        $y = 200;
+        $z = 300;
+        $add = $x + $y;
+        $sub = $z - $x;
+        $mul = $y * $z;
+        $div = $mul / $z;
+        $massage = "Hello World";
+        echo "ผลบวก  ", $x , " + ", $y, " = " ,$add;            echo"<br />";
+        echo "ผลลบ  ", $z , " - ", $x, " = " ,$sub;            echo"<br />";
+        echo "ผลคูณ  ", $y , " * ", $z, " = " ,$mul;            echo"<br />";
+        echo "ผลหาร  ", $mul , " / ", $z, " = " ,$div;            echo"<br />";
+        echo $massage;        echo"<br />";
+    
+    
+    
+    ?>
+</body>
+</html>
